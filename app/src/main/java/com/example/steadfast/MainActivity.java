0e1a14e4787b6d1658e023a0f95f8ab99fea6889
Cannoninq <220stephen@gmail.com>
2023-01-27 15:08:24 -0500

    Added Login Page

--- app/src/main/java/com/example/steadfast/MainActivity.java
+++ app/src/main/java/com/example/steadfast/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
         setContentView(R.layout.activity_main);
 
         //Initialize the ListView and set the adapter
