7e4c719bb24f1b7fbc90163899389193836df2b3
BuildTools <unconfigured@null.spigotmc.org>
2023-02-09 00:16:50 -0500

    Added Functional LoginPage

---
+++ app/src/main/java/com/example/steadfast/LoginMenu.java
@@ -0,0 +1,101 @@
+package com.example.steadfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Button;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class LoginMenu extends AppCompatActivity {
+
+    Button buttonRegister;
+    Button login;
+    TextInputEditText textInputEditTextEmail, textInputEditTextPassword;
+    ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+        login = findViewById(R.id.loginbtn);
+        buttonRegister = findViewById(R.id.registerbutton);
+        textInputEditTextEmail = findViewById(R.id.email);
+        textInputEditTextPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progress);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(textInputEditTextEmail.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+
+                if (!email.equals("") && !password.equals("")) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+                            field[0] = "email";
+                            field[1] = "password";
+                            //Creating array for data
+                            String[] data = new String[2];
+                            data[0] = email;
+                            data[1] = password;
+                            PutData putData = new PutData("http://steadfastfitness.online/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    //End ProgressBar (Set visibility to GONE)
+                                    Log.i("PutData", result);
+                                    if (result.equals("Login Success")) {
+                                        Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Email Or Password Incorrect", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.register_page);
+            }
+        });
+    }
+}
