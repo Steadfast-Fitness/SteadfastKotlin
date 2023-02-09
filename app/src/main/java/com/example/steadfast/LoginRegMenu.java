0bac13aa0b5e09a26368cbece58ed6e35ce2c50e
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 22:18:55 -0500

    Added Functional Register Page

--- app/src/main/java/com/example/steadfast/LoginRegMenu.java
+++ app/src/main/java/com/example/steadfast/LoginRegMenu.java
@@ -3,13 +3,20 @@ package com.example.steadfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 
 public class LoginRegMenu extends AppCompatActivity {
@@ -17,12 +24,14 @@ public class LoginRegMenu extends AppCompatActivity {
     private SupplementAdapter mSupplementAdapter;
     Button buttonRegister;
     Button buttonReg;
-
+    Button buttonRegisters;
     Button loginbtn;
-
+    EditText email;
     EditText username;
     EditText password;
     TextView welcomename;
+    TextInputEditText textInputEditTextUsername, textInputEditTextEmail, textInputEditTextPassword, textInputEditTextReEnterPassword;
+    ProgressBar progressBar;
 
 
     @Override
@@ -34,9 +43,9 @@ public class LoginRegMenu extends AppCompatActivity {
         //buttonRegister = findViewById(R.id.registerbtn);
         buttonReg = findViewById(R.id.registerbutton);
         loginbtn = findViewById(R.id.loginbtn);
-
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+
 
         //Login Info
         loginbtn.setOnClickListener(new View.OnClickListener() {
@@ -50,13 +59,76 @@ public class LoginRegMenu extends AppCompatActivity {
 
 
         });
-
         //Register Button
         buttonReg.setOnClickListener(new View.OnClickListener() {
                     @Override
             public void onClick(View view) {
                 setContentView(R.layout.register_page);
                 buttonRegister = findViewById(R.id.signinbtn);
+                buttonRegisters = findViewById(R.id.registerbtn);
+                textInputEditTextUsername = findViewById(R.id.username);
+                textInputEditTextEmail = findViewById(R.id.email);
+                textInputEditTextPassword = findViewById(R.id.password);
+                textInputEditTextReEnterPassword = findViewById(R.id.reenter_password);
+                progressBar = findViewById(R.id.progress);
+
+                buttonRegisters.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String username, email, password, reEnter;
+                        username = String.valueOf(textInputEditTextUsername.getText());
+                        email = String.valueOf(textInputEditTextEmail.getText());
+                        password = String.valueOf(textInputEditTextPassword.getText());
+                        reEnter = String.valueOf(textInputEditTextReEnterPassword.getText());
+
+                        if(!username.equals("") && !email.equals("") && !password.equals("")) {
+                            if (!reEnter.equals(password)) {
+                                Toast.makeText(getApplicationContext(), "Passwords Don't Match", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                progressBar.setVisibility(View.VISIBLE);
+                                //Start ProgressBar first (Set visibility VISIBLE)
+                                Handler handler = new Handler();
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        //Starting Write and Read data with URL
+                                        //Creating array for parameters
+                                        String[] field = new String[3];
+                                        field[0] = "username";
+                                        field[1] = "email";
+                                        field[2] = "password";
+                                        //Creating array for data
+                                        String[] data = new String[3];
+                                        data[0] = username;
+                                        data[1] = email;
+                                        data[2] = password;
+                                        PutData putData = new PutData("http://steadfastfitness.online/signup.php", "POST", field, data);
+                                        if (putData.startPut()) {
+                                            if (putData.onComplete()) {
+                                                progressBar.setVisibility(View.GONE);
+                                                String result = putData.getResult();
+                                                //End ProgressBar (Set visibility to GONE)
+                                                Log.i("PutData", result);
+                                                if (result.equals("Registration Successful")) {
+                                                    setContentView(R.layout.activity_main);
+                                                } else {
+                                                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        }
+                                        //End Write and Read data with URL
+                                    }
+                                });
+                            }
+                        }else{
+                            Toast.makeText(getApplicationContext(), "All Fields Are Required", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
 
                 buttonRegister.setOnClickListener(new View.OnClickListener() {
 
@@ -76,6 +148,9 @@ public class LoginRegMenu extends AppCompatActivity {
 
 
         });
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/steadfast/Register.java
@@ -0,0 +1,10 @@
+package com.example.steadfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Register extends AppCompatActivity {
+
+    TextInputEditText textInputEditTextUsername, textInputEditTextEmail, textInputEditTextPassword;
+}
